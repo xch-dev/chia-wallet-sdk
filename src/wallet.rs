524c3e3b7d37e921e2450473ff7ef023062a1674
Rigidity <me@rigidnetwork.com>
2024-01-21 20:05:39 -0500

    Remove wallet trait

--- src/wallet.rs
+++ src/wallet.rs
@@ -1,7 +1,3 @@
-use std::future::Future;
-
-use chia_protocol::Coin;
-
 mod coin_selection;
 mod coin_store;
 mod derivation_store;
@@ -11,32 +7,3 @@ pub use coin_selection::*;
 pub use coin_store::*;
 pub use derivation_store::*;
 pub use sync::*;
-
-/// A wallet is responsible for managing coins.
-pub trait Wallet: Sync {
-    /// Returns the coins that are spendable by this wallet.
-    fn spendable_coins(&self) -> impl Future<Output = Vec<Coin>> + Send;
-
-    /// Returns the total amount of coins that are spendable by this wallet.
-    fn spendable_balance(&self) -> impl Future<Output = u64> + Send {
-        async {
-            self.spendable_coins()
-                .await
-                .iter()
-                .fold(0, |balance, coin| balance + coin.amount)
-        }
-    }
-
-    /// Returns the coins that are either spendable or pending in this wallet.
-    fn pending_coins(&self) -> impl Future<Output = Vec<Coin>> + Send;
-
-    /// Returns the total amount of coins that are either spendable or pending in this wallet.
-    fn pending_balance(&self) -> impl Future<Output = u64> + Send {
-        async {
-            self.pending_coins()
-                .await
-                .iter()
-                .fold(0, |balance, coin| balance + coin.amount)
-        }
-    }
-}
