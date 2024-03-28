075f488037930ffda5b94b1738085fa7a18d10a9
Rigidity <me@rigidnetwork.com>
2024-03-28 13:33:58 -0400

    Rename error file

--- src/sqlite.rs
+++ src/sqlite.rs
@@ -1,11 +1,11 @@
 mod cat_coin_store;
 mod coin_store;
-mod sqlite_error;
+mod error;
 mod transaction_store;
 
 pub use cat_coin_store::*;
 pub use coin_store::*;
-pub use sqlite_error::*;
+pub use error::*;
 pub use transaction_store::*;
 
 type Result<T> = std::result::Result<T, SqliteError>;
