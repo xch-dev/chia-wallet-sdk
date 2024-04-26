4c5705b24ce345592937d6f2a75bbcfd5f8eb6cc
Rigidity <me@rigidnetwork.com>
2024-04-26 11:41:10 -0400

    Add WalletSimulator

--- src/wallet.rs
+++ src/wallet.rs
@@ -1,7 +1,9 @@
 mod coin_selection;
 mod required_signature;
 mod signer;
+mod wallet_simulator;
 
 pub use coin_selection::*;
 pub use required_signature::*;
 pub use signer::*;
+pub use wallet_simulator::*;
