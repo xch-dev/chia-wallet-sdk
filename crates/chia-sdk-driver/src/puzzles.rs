4f1cd100d9709ada2c8ed0174ee1c734f49f0732
Rigidity <me@rigidnetwork.com>
2024-05-18 13:04:31 -0400

    Remove offer

--- crates/chia-sdk-driver/src/puzzles.rs
+++ crates/chia-sdk-driver/src/puzzles.rs
@@ -1,13 +1,11 @@
 mod cat;
 mod did;
 mod nft;
-mod offer;
 mod singleton;
 mod standard;
 
 pub use cat::*;
 pub use did::*;
 pub use nft::*;
-pub use offer::*;
 pub use singleton::*;
 pub use standard::*;
