2c61e70ffbc377785aba4c928021fbe3c3124c3c
yakuhito <y@kuhi.to>
2024-07-25 17:04:17 +0300

    time to take a break

--- crates/chia-sdk-parser/src/puzzles.rs
+++ crates/chia-sdk-parser/src/puzzles.rs
@@ -1,13 +1,13 @@
-mod cat;
-mod did;
-mod nft;
-mod nft_ownership_layer;
-mod nft_state_layer;
-mod singleton;
+// mod cat;
+// mod did;
+// mod nft;
+// mod nft_ownership_layer;
+// mod nft_state_layer;
+// mod singleton;
 
-pub use cat::*;
-pub use did::*;
-pub use nft::*;
-pub use nft_ownership_layer::*;
-pub use nft_state_layer::*;
-pub use singleton::*;
+// pub use cat::*;
+// pub use did::*;
+// pub use nft::*;
+// pub use nft_ownership_layer::*;
+// pub use nft_state_layer::*;
+// pub use singleton::*;
--- crates/chia-sdk-parser/src/puzzles/singleton.rs
+++ crates/chia-sdk-parser/src/puzzles/singleton.rs
@@ -1,51 +1,51 @@
-use chia_protocol::{Bytes32, Coin};
-use chia_puzzles::{
-    singleton::{SingletonArgs, SINGLETON_LAUNCHER_PUZZLE_HASH, SINGLETON_TOP_LAYER_PUZZLE_HASH},
-    LineageProof,
-};
-use clvm_traits::FromClvm;
-use clvmr::{Allocator, NodePtr};
+// use chia_protocol::{Bytes32, Coin};
+// use chia_puzzles::{
+//     singleton::{SingletonArgs, SINGLETON_LAUNCHER_PUZZLE_HASH, SINGLETON_TOP_LAYER_PUZZLE_HASH},
+//     LineageProof,
+// };
+// use clvm_traits::FromClvm;
+// use clvmr::{Allocator, NodePtr};
 
-use crate::{ParseError, Puzzle};
+// use crate::{ParseError, Puzzle};
 
-#[derive(Debug, Clone, Copy)]
-pub struct SingletonPuzzle {
-    pub launcher_id: Bytes32,
-    pub inner_puzzle: Puzzle,
-}
+// #[derive(Debug, Clone, Copy)]
+// pub struct SingletonPuzzle {
+//     pub launcher_id: Bytes32,
+//     pub inner_puzzle: Puzzle,
+// }
 
-impl SingletonPuzzle {
-    pub fn parse(
-        allocator: &Allocator,
-        puzzle: &Puzzle,
-    ) -> Result<Option<SingletonPuzzle>, ParseError> {
-        let Some(puzzle) = puzzle.as_curried() else {
-            return Ok(None);
-        };
+// impl SingletonPuzzle {
+//     pub fn parse(
+//         allocator: &Allocator,
+//         puzzle: &Puzzle,
+//     ) -> Result<Option<SingletonPuzzle>, ParseError> {
+//         let Some(puzzle) = puzzle.as_curried() else {
+//             return Ok(None);
+//         };
 
-        if puzzle.mod_hash != SINGLETON_TOP_LAYER_PUZZLE_HASH {
-            return Ok(None);
-        }
+//         if puzzle.mod_hash != SINGLETON_TOP_LAYER_PUZZLE_HASH {
+//             return Ok(None);
+//         }
 
-        let args = SingletonArgs::<NodePtr>::from_clvm(allocator, puzzle.args)?;
+//         let args = SingletonArgs::<NodePtr>::from_clvm(allocator, puzzle.args)?;
 
-        if args.singleton_struct.mod_hash != SINGLETON_TOP_LAYER_PUZZLE_HASH.into()
-            || args.singleton_struct.launcher_puzzle_hash != SINGLETON_LAUNCHER_PUZZLE_HASH.into()
-        {
-            return Err(ParseError::InvalidSingletonStruct);
-        }
+//         if args.singleton_struct.mod_hash != SINGLETON_TOP_LAYER_PUZZLE_HASH.into()
+//             || args.singleton_struct.launcher_puzzle_hash != SINGLETON_LAUNCHER_PUZZLE_HASH.into()
+//         {
+//             return Err(ParseError::InvalidSingletonStruct);
+//         }
 
-        Ok(Some(SingletonPuzzle {
-            launcher_id: args.singleton_struct.launcher_id,
-            inner_puzzle: Puzzle::parse(allocator, args.inner_puzzle),
-        }))
-    }
+//         Ok(Some(SingletonPuzzle {
+//             launcher_id: args.singleton_struct.launcher_id,
+//             inner_puzzle: Puzzle::parse(allocator, args.inner_puzzle),
+//         }))
+//     }
 
-    pub fn lineage_proof(&self, parent_coin: Coin) -> LineageProof {
-        LineageProof {
-            parent_parent_coin_id: parent_coin.parent_coin_info,
-            parent_inner_puzzle_hash: self.inner_puzzle.curried_puzzle_hash().into(),
-            parent_amount: parent_coin.amount,
-        }
-    }
-}
+//     pub fn lineage_proof(&self, parent_coin: Coin) -> LineageProof {
+//         LineageProof {
+//             parent_parent_coin_id: parent_coin.parent_coin_info,
+//             parent_inner_puzzle_hash: self.inner_puzzle.curried_puzzle_hash().into(),
+//             parent_amount: parent_coin.amount,
+//         }
+//     }
+// }
