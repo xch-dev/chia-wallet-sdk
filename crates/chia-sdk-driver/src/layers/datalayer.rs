16c0c22637d44451ff4a2075fc1f18a2440f7669
yakuhito <y@kuhi.to>
2024-08-19 12:45:53 +0300

    DL metadata updater tests

--- crates/chia-sdk-driver/src/layers/datalayer.rs
+++ crates/chia-sdk-driver/src/layers/datalayer.rs
@@ -20,11 +20,16 @@ pub const DL_METADATA_UPDATER_PUZZLE_HASH: TreeHash = TreeHash::new(hex!(
     "
 ));
 
+#[allow(unused_imports)]
 mod tests {
-    use crate::{
-        DELEGATION_LAYER_PUZZLE, DELEGATION_LAYER_PUZZLE_HASH, DL_METADATA_UPDATER_PUZZLE,
-        DL_METADATA_UPDATER_PUZZLE_HASH, WRITER_FILTER_PUZZLE, WRITER_FILTER_PUZZLE_HASH,
-    };
+    use clvm_traits::ToClvm;
+    use clvm_utils::tree_hash;
+    use clvmr::serde::node_from_bytes;
+    use rstest::rstest;
+
+    use crate::SpendContext;
+
+    use super::*;
 
     // not exported, so I had to copy-paste
     // use chia_puzzles::assert_puzzle_hash;
@@ -43,5 +48,31 @@ mod tests {
         assert_puzzle_hash!(DELEGATION_LAYER_PUZZLE => DELEGATION_LAYER_PUZZLE_HASH);
         assert_puzzle_hash!(WRITER_FILTER_PUZZLE => WRITER_FILTER_PUZZLE_HASH);
         assert_puzzle_hash!(DL_METADATA_UPDATER_PUZZLE => DL_METADATA_UPDATER_PUZZLE_HASH);
+    }
+
+    // tests that DL metadata updater indeed returns the third argument
+    #[rstest]
+    #[case::string(hex!("8379616b").to_vec())] // run -d '"yak"'
+    #[case::atom(hex!("ff018379616b").to_vec())] // run -d '(mod () "yak"))'
+    #[case::one_item_list(hex!("ff01ff0180").to_vec())] // run -d '(mod () (list 1)))'
+    #[case::multiple_item_list(hex!("ff01ff01ff02ff0380").to_vec())] // run -d '(mod () (list 1 2 3)))'
+    #[case::lists_within_list(hex!("ff01ff01ffff02ff0380ffff04ff0580ffff060780").to_vec())] // run -d '(mod () (list 1 (list 2 3) (list 4 5) (c 6 7))))'
+    fn test_dl_metadata_updater_puzzle(#[case] third_arg: Vec<u8>) -> Result<(), ()> {
+        let mut ctx = SpendContext::new();
+
+        let third_arg_ptr = node_from_bytes(ctx.allocator_mut(), &third_arg).unwrap();
+        let solution_ptr = vec![ctx.allocator().nil(), ctx.allocator().nil(), third_arg_ptr]
+            .to_clvm(ctx.allocator_mut())
+            .unwrap();
+
+        let puzzle_ptr = node_from_bytes(ctx.allocator_mut(), &DL_METADATA_UPDATER_PUZZLE).unwrap();
+        let output = ctx.run(puzzle_ptr, solution_ptr).unwrap();
+
+        assert_eq!(
+            tree_hash(ctx.allocator(), output),
+            tree_hash(ctx.allocator(), third_arg_ptr),
+        );
+
+        Ok(())
     }
 }
