a279fb8c9bcca18cd3ecd6c201bb66ebbb480112
yakuhito <y@kuhi.to>
2024-08-19 15:42:19 +0300

    delegation layer

--- crates/chia-sdk-driver/src/layers/datalayer/delegation_layer.rs
+++ crates/chia-sdk-driver/src/layers/datalayer/delegation_layer.rs
@@ -1,7 +1,83 @@
 use chia_protocol::Bytes32;
 use clvm_traits::{apply_constants, FromClvm, ToClvm};
 use clvm_utils::{CurriedProgram, ToTreeHash, TreeHash};
+use clvmr::{Allocator, NodePtr};
 use hex_literal::hex;
+
+use crate::{DriverError, Layer, Puzzle, SpendContext};
+
+#[allow(clippy::doc_markdown)]
+/// The Delegation [`Layer`] is used to enable DataLayer delegation capabilities
+/// For more information, see CHIP-0035.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct DelegationLayer {
+    // Launcher ID of the singleton outer layer - used as default hint when re-creating layer
+    pub launcher_id: Bytes32,
+    // Puzzle hash of the 'owner' puzzle (usually a standard puzzle)
+    pub owner_puzzle_hash: Bytes32,
+    // Merkle root corresponding to the tree of delegated puzzles
+    pub merkle_root: Bytes32,
+}
+
+impl DelegationLayer {
+    pub fn new(launcher_id: Bytes32, owner_puzzle_hash: Bytes32, merkle_root: Bytes32) -> Self {
+        Self {
+            launcher_id,
+            owner_puzzle_hash,
+            merkle_root,
+        }
+    }
+}
+
+impl Layer for DelegationLayer {
+    type Solution = DelegationLayerSolution<NodePtr, NodePtr>;
+
+    fn parse_puzzle(allocator: &Allocator, puzzle: Puzzle) -> Result<Option<Self>, DriverError> {
+        let Some(puzzle) = puzzle.as_curried() else {
+            return Ok(None);
+        };
+
+        if puzzle.mod_hash != DELEGATION_LAYER_PUZZLE_HASH {
+            return Ok(None);
+        }
+
+        let args = DelegationLayerArgs::from_clvm(allocator, puzzle.args)?;
+
+        Ok(Some(Self {
+            launcher_id: args.launcher_id,
+            owner_puzzle_hash: args.owner_puzzle_hash,
+            merkle_root: args.merkle_root,
+        }))
+    }
+
+    fn parse_solution(
+        allocator: &Allocator,
+        solution: NodePtr,
+    ) -> Result<Self::Solution, DriverError> {
+        DelegationLayerSolution::<NodePtr, NodePtr>::from_clvm(allocator, solution)
+            .map_err(DriverError::FromClvm)
+    }
+
+    fn construct_puzzle(&self, ctx: &mut SpendContext) -> Result<NodePtr, DriverError> {
+        let curried = CurriedProgram {
+            program: ctx.did_inner_puzzle()?,
+            args: DelegationLayerArgs::new(
+                self.launcher_id,
+                self.owner_puzzle_hash,
+                self.merkle_root,
+            ),
+        };
+        ctx.alloc(&curried)
+    }
+
+    fn construct_solution(
+        &self,
+        ctx: &mut SpendContext,
+        solution: Self::Solution,
+    ) -> Result<NodePtr, DriverError> {
+        ctx.alloc(&solution)
+    }
+}
 
 pub const DELEGATION_LAYER_PUZZLE: [u8; 1027] = hex!(
     "
@@ -45,16 +121,16 @@ pub const DELEGATION_LAYER_PUZZLE_HASH: TreeHash = TreeHash::new(hex!(
 pub struct DelegationLayerArgs {
     pub mod_hash: Bytes32,
     pub launcher_id: Bytes32,
-    pub inner_puzzle_hash: Bytes32,
+    pub owner_puzzle_hash: Bytes32,
     pub merkle_root: Bytes32,
 }
 
 impl DelegationLayerArgs {
-    pub fn new(launcher_id: Bytes32, inner_puzzle_hash: Bytes32, merkle_root: Bytes32) -> Self {
+    pub fn new(launcher_id: Bytes32, owner_puzzle_hash: Bytes32, merkle_root: Bytes32) -> Self {
         Self {
             mod_hash: DELEGATION_LAYER_PUZZLE_HASH.into(),
             launcher_id,
-            inner_puzzle_hash,
+            owner_puzzle_hash,
             merkle_root,
         }
     }
@@ -63,7 +139,7 @@ impl DelegationLayerArgs {
 impl DelegationLayerArgs {
     pub fn curry_tree_hash(
         launcher_id: Bytes32,
-        inner_puzzle_hash: Bytes32,
+        owner_puzzle_hash: Bytes32,
         merkle_root: Bytes32,
     ) -> TreeHash {
         CurriedProgram {
@@ -71,7 +147,7 @@ impl DelegationLayerArgs {
             args: DelegationLayerArgs {
                 mod_hash: DELEGATION_LAYER_PUZZLE_HASH.into(),
                 launcher_id,
-                inner_puzzle_hash,
+                owner_puzzle_hash,
                 merkle_root,
             },
         }
