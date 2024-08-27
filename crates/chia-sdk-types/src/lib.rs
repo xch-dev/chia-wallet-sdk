454fa9e3ff1de9079ea7249061d2408e7f37558f
Rigidity <me@rigidnetwork.com>
2024-08-27 15:47:19 -0400

    Remove non_epehemeral

--- crates/chia-sdk-types/src/lib.rs
+++ crates/chia-sdk-types/src/lib.rs
@@ -1,15 +1,11 @@
 mod condition;
-mod condition_error;
 mod conditions;
 mod constants;
-mod non_ephemeral_coins;
 mod puzzles;
 mod run_puzzle;
 
 pub use condition::*;
-pub use condition_error::*;
 pub use conditions::*;
 pub use constants::*;
-pub use non_ephemeral_coins::*;
 pub use puzzles::*;
 pub use run_puzzle::*;
