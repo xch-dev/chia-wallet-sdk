e71d5135b255d665d33a9f4d383806c8d7b76a9d
Rigidity <me@rigidnetwork.com>
2024-12-04 17:17:11 -0500

    Finish initial vault puzzles

--- crates/chia-sdk-types/src/puzzles/vault.rs
+++ crates/chia-sdk-types/src/puzzles/vault.rs
@@ -1,7 +1,13 @@
+mod delegated_feeder;
+mod index_wrapper;
+mod restrictions;
 mod vault_1_of_n;
 mod vault_m_of_n;
 mod vault_n_of_n;
 
+pub use delegated_feeder::*;
+pub use index_wrapper::*;
+pub use restrictions::*;
 pub use vault_1_of_n::*;
 pub use vault_m_of_n::*;
 pub use vault_n_of_n::*;
---
+++ crates/chia-sdk-types/src/puzzles/vault/index_wrapper.rs
@@ -0,0 +1,32 @@
+use clvm_traits::{FromClvm, ToClvm};
+use clvm_utils::TreeHash;
+use hex_literal::hex;
+
+use crate::Mod;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, ToClvm, FromClvm)]
+#[clvm(curry)]
+pub struct IndexWrapperArgs<I> {
+    pub index: u32,
+    pub inner_puzzle: I,
+}
+
+impl<I> IndexWrapperArgs<I> {
+    pub fn new(index: u32, inner_puzzle: I) -> Self {
+        Self {
+            index,
+            inner_puzzle,
+        }
+    }
+}
+
+impl<I> Mod for IndexWrapperArgs<I> {
+    const MOD_REVEAL: &[u8] = &INDEX_WRAPPER;
+    const MOD_HASH: TreeHash = INDEX_WRAPPER_HASH;
+}
+
+pub const INDEX_WRAPPER: [u8; 7] = hex!("ff02ff05ff0780");
+
+pub const INDEX_WRAPPER_HASH: TreeHash = TreeHash::new(hex!(
+    "847d971ef523417d555ea9854b1612837155d34d453298defcd310774305f657"
+));
