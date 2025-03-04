d0255cad24e507f7895fcf97631fce0c26d242db
Matthew Howard <m.o.howard97@gmail.com>
2025-03-04 17:49:02 +0000

    add remark generation function and add to test

--- crates/chia-sdk-driver/src/primitives/clawback.rs
+++ crates/chia-sdk-driver/src/primitives/clawback.rs
@@ -22,6 +22,7 @@ pub struct VersionedBlob {
 
 #[streamable]
 #[derive(Copy)]
+// this struct is an unfortunate hack in order to get the streamable bytes which are used on chain for recreating ourself
 pub struct ClawbackMetadata {
     timelock: u64,
     sender_puzzle_hash: Bytes32,
@@ -41,7 +42,7 @@ pub struct Clawback {
 impl Clawback {
     pub fn parse_children(
         allocator: &mut Allocator,
-        parent_puzzle: Puzzle,
+        parent_puzzle: Puzzle, // this could be any puzzle type
         parent_solution: NodePtr,
     ) -> Result<Option<Vec<Self>>, DriverError>
     where
@@ -49,7 +50,7 @@ impl Clawback {
     {
         let output = run_puzzle(allocator, parent_puzzle.ptr(), parent_solution)?;
         let conditions = Vec::<Condition>::from_clvm(allocator, output)?;
-
+        println!("DEBUG 1 - conditions: {:?}", conditions);
         let mut outputs = Vec::<Clawback>::new();
         let mut metadatas = Vec::<ClawbackMetadata>::new();
         let mut puzhashes = Vec::<[u8; 32]>::with_capacity(conditions.len());
@@ -68,6 +69,7 @@ impl Clawback {
                             )
                             .map_err(|_| DriverError::InvalidMemo)?,
                         );
+                        println!("DEBUG 2 - metadatas: {:?}", metadatas);
                     }
                 },
                 _ => {}
@@ -83,7 +85,7 @@ impl Clawback {
                 }
             }
         }
-
+        println!("DEBUG 3 - outputs: {:?}", outputs);
         Ok(Some(outputs))
     }
 
@@ -130,6 +132,31 @@ impl Clawback {
 
     pub fn to_layer(&self) -> P2OneOfManyLayer {
         P2OneOfManyLayer::new(self.merkle_tree().root())
+    }
+
+    // this function returns the Remark condition required to hint at this clawback
+    // it should be included alongside the createcoin that creates this
+    pub fn get_remark_condition(&self) -> Result<Condition, DriverError> {
+        let mut allocator = Allocator::new();
+        let first = allocator.new_small_number(2)?; // magic number for clawback bytes dump
+        let cbm = ClawbackMetadata {
+            timelock: self.timelock.into(),
+            sender_puzzle_hash: self.sender_puzzle_hash,
+            recipient_puzzle_hash: self.recipient_puzzle_hash,
+        };
+        let vb = VersionedBlob {
+            version: 1,
+            blob: cbm.to_bytes().map_err(|_| DriverError::InvalidMemo)?.into(),
+        };
+        let bytes = allocator.new_atom(
+            vb.to_bytes()
+                .map_err(|_| DriverError::InvalidMemo)?
+                .as_ref(),
+        )?;
+        let rest = allocator.new_pair(bytes, allocator.nil())?;
+        let node_ptr = allocator.new_pair(first, rest)?;
+
+        return Ok(Condition::remark(node_ptr));
     }
 
     pub fn claim_spend(&self, ctx: &mut SpendContext, spend: Spend) -> Result<Spend, DriverError> {
@@ -194,26 +221,25 @@ mod tests {
             recipient_puzzle_hash: bob.puzzle_hash,
         };
         let clawback_puzzle_hash = clawback.to_layer().tree_hash().into();
-
-        alice_p2.spend(
-            ctx,
-            alice.coin,
-            Conditions::new().create_coin(clawback_puzzle_hash, 1, None),
-        )?;
+        let coin = alice.coin;
+        let conditions = Conditions::new().create_coin(clawback_puzzle_hash, 1, None);
+        let conditions = conditions.with(clawback.get_remark_condition()?);
+        println!("DEBUG 0 -  CONDITION INTENTIONS: {:?}", conditions);
+        alice_p2.spend(ctx, coin, conditions)?;
         let mut allocator = Allocator::new();
         let cs = ctx.take();
 
-        let clawback_coin = Coin::new(alice.coin.coin_id(), clawback_puzzle_hash, 1);
+        let clawback_coin = Coin::new(coin.coin_id(), clawback_puzzle_hash, 1);
 
         sim.spend_coins(cs, &[alice.sk])?;
 
         let puzzle_reveal = sim
-            .puzzle_reveal(alice.coin.coin_id())
+            .puzzle_reveal(coin.coin_id())
             .expect("missing puzzle")
             .to_clvm(&mut allocator)?;
 
         let solution = sim
-            .solution(alice.coin.coin_id())
+            .solution(coin.coin_id())
             .expect("missing solution")
             .to_clvm(&mut allocator)?;
 
