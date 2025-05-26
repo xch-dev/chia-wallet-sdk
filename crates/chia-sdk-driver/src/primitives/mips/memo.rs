3071191dce7d7555cdcff16fe978c453b66385b9
Rigidity <me@rigidnetwork.com>
2025-05-26 16:15:25 -0400

    Temp memos

--- crates/chia-sdk-driver/src/primitives/mips/memo.rs
+++ crates/chia-sdk-driver/src/primitives/mips/memo.rs
@@ -1,4 +1,13 @@
+use chia_bls::PublicKey;
 use chia_protocol::Bytes32;
+use chia_sdk_types::{
+    puzzles::{
+        BlsMember, FixedPuzzleMember, PasskeyMember, PasskeyMemberPuzzleAssert, Secp256k1Member,
+        Secp256k1MemberPuzzleAssert, Secp256r1Member, Secp256r1MemberPuzzleAssert, SingletonMember,
+    },
+    Mod,
+};
+use chia_secp::{K1PublicKey, R1PublicKey};
 use clvm_traits::{apply_constants, FromClvm, ToClvm};
 use clvmr::NodePtr;
 
@@ -58,23 +67,93 @@ impl RestrictionMemo<NodePtr> {
 #[derive(Debug, Clone, PartialEq, Eq, ToClvm, FromClvm)]
 #[clvm(list)]
 pub enum MemoKind<T = NodePtr> {
-    Member {
-        puzzle_hash: Bytes32,
-        memo: T,
-    },
+    Member(#[clvm(rest)] MemberMemo<T>),
     MofN {
         required: usize,
         items: Vec<InnerPuzzleMemo<T>>,
     },
 }
 
-impl MemoKind<NodePtr> {
-    pub fn member(puzzle_hash: Bytes32, memo: NodePtr) -> Self {
-        Self::Member { puzzle_hash, memo }
+#[derive(Debug, Clone, PartialEq, Eq, ToClvm, FromClvm)]
+#[clvm(list)]
+pub struct MemberMemo<T = NodePtr> {
+    pub puzzle_hash: Bytes32,
+    pub memo: T,
+}
+
+impl MemberMemo<NodePtr> {
+    pub fn new(puzzle_hash: Bytes32, memo: NodePtr) -> Self {
+        Self { puzzle_hash, memo }
     }
 
-    pub fn mofn(required: usize, items: Vec<InnerPuzzleMemo>) -> Self {
-        Self::MofN { required, items }
+    pub fn k1(public_key: K1PublicKey, fast_forward: bool) -> Self {
+        Self::new(
+            if fast_forward {
+                Secp256k1MemberPuzzleAssert::new(public_key).curry_tree_hash()
+            } else {
+                Secp256k1Member::new(public_key).curry_tree_hash()
+            }
+            .into(),
+            NodePtr::NIL,
+        )
+    }
+
+    pub fn r1(public_key: R1PublicKey, fast_forward: bool) -> Self {
+        Self::new(
+            if fast_forward {
+                Secp256r1MemberPuzzleAssert::new(public_key).curry_tree_hash()
+            } else {
+                Secp256r1Member::new(public_key).curry_tree_hash()
+            }
+            .into(),
+            NodePtr::NIL,
+        )
+    }
+
+    pub fn bls(public_key: PublicKey) -> Self {
+        Self::new(
+            BlsMember::new(public_key).curry_tree_hash().into(),
+            NodePtr::NIL,
+        )
+    }
+
+    pub fn passkey(public_key: R1PublicKey, fast_forward: bool) -> Self {
+        Self::new(
+            if fast_forward {
+                PasskeyMemberPuzzleAssert::new(public_key).curry_tree_hash()
+            } else {
+                PasskeyMember::new(public_key).curry_tree_hash()
+            }
+            .into(),
+            NodePtr::NIL,
+        )
+    }
+
+    pub fn singleton(launcher_id: Bytes32) -> Self {
+        Self::new(
+            SingletonMember::new(launcher_id).curry_tree_hash().into(),
+            NodePtr::NIL,
+        )
+    }
+
+    pub fn fixed_puzzle(puzzle_hash: Bytes32) -> Self {
+        Self::new(
+            FixedPuzzleMember::new(puzzle_hash).curry_tree_hash().into(),
+            NodePtr::NIL,
+        )
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, ToClvm, FromClvm)]
+#[clvm(list)]
+pub struct MofNMemo<T = NodePtr> {
+    pub required: usize,
+    pub items: Vec<InnerPuzzleMemo<T>>,
+}
+
+impl MofNMemo<NodePtr> {
+    pub fn new(required: usize, items: Vec<InnerPuzzleMemo>) -> Self {
+        Self { required, items }
     }
 }
 
