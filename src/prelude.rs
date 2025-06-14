8a2f2069d6fcd5c20a4b911456b112cc292faa95
Rigidity <me@rigidnetwork.com>
2025-06-14 00:53:53 -0400

    Fix

--- src/prelude.rs
+++ src/prelude.rs
@@ -5,8 +5,8 @@ pub use clvm_utils::{CurriedProgram, ToTreeHash, TreeHash};
 pub use clvmr::{Allocator, NodePtr};
 
 pub use chia_sdk_driver::{
-    Cat, CatSpend, Did, DidInfo, DidOwner, DriverError, Launcher, MetadataUpdate, Nft, NftInfo,
-    NftMint,
+    Cat, CatSpend, Did, DidInfo, DriverError, Launcher, MetadataUpdate, Nft, NftInfo, NftMint,
+    NftOwner,
 };
 pub use chia_sdk_test::{BlsPair, BlsPairWithCoin, K1Pair, R1Pair, Simulator, SimulatorError};
 pub use chia_sdk_types::{
