0fbdfaa6ddb214b1396734e9e30b83f3b5e7191a
Rigidity <me@rigidnetwork.com>
2025-06-17 15:54:29 -0400

    Test CAT parsing

--- napi/__test__/cats.spec.ts
+++ napi/__test__/cats.spec.ts
@@ -1,6 +1,14 @@
 import test from "ava";
 
-import { Cat, CatInfo, CatSpend, Clvm, Coin, Simulator } from "../index.js";
+import {
+  Cat,
+  CatInfo,
+  CatSpend,
+  Clvm,
+  Coin,
+  Constants,
+  Simulator,
+} from "../index.js";
 
 test("issues and spends a cat", (t) => {
   const sim = new Simulator();
@@ -56,4 +64,35 @@ test("issues and spends a cat", (t) => {
   sim.spendCoins(clvm.coinSpends(), [alice.sk]);
 
   t.true(true);
+});
+
+test("parses a cat puzzle", (t) => {
+  const clvm = new Clvm();
+
+  const settlementPuzzle = clvm.deserialize(Constants.settlementPayment());
+  const catPuzzle = clvm.deserialize(Constants.catPuzzle());
+
+  const assetId = Buffer.from("00".repeat(32), "hex");
+
+  const puzzle = catPuzzle
+    .curry([
+      clvm.alloc(Constants.catPuzzleHash()),
+      clvm.alloc(assetId),
+      settlementPuzzle,
+    ])
+    .puzzle();
+
+  const parsed = puzzle.parseCat();
+  if (!parsed?.p2Puzzle) throw new Error("Failed to parse cat");
+
+  t.is(parsed.info.assetId.toString("hex"), assetId.toString("hex"));
+  t.is(parsed.info.hiddenPuzzleHash, null);
+  t.is(
+    parsed.info.p2PuzzleHash.toString("hex"),
+    Constants.settlementPaymentHash().toString("hex")
+  );
+  t.is(
+    parsed.p2Puzzle?.program.treeHash().toString("hex"),
+    Constants.settlementPaymentHash().toString("hex")
+  );
 });
