6998f621f42f0ea1b950b568049b45f2967edd5f
Yak <y@kuhi.to>
2025-07-26 16:49:40 -0400

    new puzzles but it's not enough is it

--- crates/chia-sdk-types/src/puzzles/action_layer/action_layer_puzzle.rs
+++ crates/chia-sdk-types/src/puzzles/action_layer/action_layer_puzzle.rs
@@ -7,35 +7,40 @@ use hex_literal::hex;
 
 use crate::{MerkleProof, Mod};
 
-pub const ACTION_LAYER_PUZZLE: [u8; 670] = hex!(
+pub const ACTION_LAYER_PUZZLE: [u8; 808] = hex!(
     "
-    ff02ffff01ff02ff05ffff04ff0bffff04ff17ffff04ffff02ff0affff04ff02
+    ff02ffff01ff02ff05ffff04ff0bffff04ff17ffff04ffff02ff16ffff04ff02
     ffff04ff2fffff04ff80ffff04ffff04ffff04ff80ff1780ff8080ffff04ffff
-    02ff0cffff04ff02ffff04ff0bffff04ff2fffff04ff80ffff04ff5fff808080
-    80808080ffff04ff81bfff8080808080808080ffff04ff82017fff8080808080
-    80ffff04ffff01ffffff02ffff03ffff09ff05ff1380ffff01ff0101ffff01ff
-    02ff08ffff04ff02ffff04ff05ffff04ff1bff808080808080ff0180ff02ffff
-    03ff2fffff01ff02ffff03ffff02ffff03ff81cfffff01ff09ff05ffff02ff1e
-    ffff04ff02ffff04ffff0bffff0101ffff02ff16ffff04ff02ffff04ffff02ff
-    818fff0b80ff8080808080ffff04ff81cfff808080808080ffff01ff02ff08ff
-    ff04ff02ffff04ff818fffff04ff17ff808080808080ff0180ffff01ff02ff0c
-    ffff04ff02ffff04ff05ffff04ff0bffff04ffff04ff818fff1780ffff04ff6f
-    ff80808080808080ffff01ff088080ff0180ffff011780ff0180ffff02ffff03
-    ff2fffff01ff02ff0affff04ff02ffff04ff05ffff04ffff04ff37ff0b80ffff
-    04ffff02ffff02ff4fff0580ffff04ff27ffff04ff819fff80808080ffff04ff
-    6fffff04ff81dfff8080808080808080ffff01ff04ff27ffff04ff37ff0b8080
-    80ff0180ffff02ffff03ffff07ff0580ffff01ff0bffff0102ffff02ff16ffff
-    04ff02ffff04ff09ff80808080ffff02ff16ffff04ff02ffff04ff0dff808080
-    8080ffff01ff0bffff0101ff058080ff0180ff02ffff03ff1bffff01ff02ff1e
-    ffff04ff02ffff04ffff02ffff03ffff18ffff0101ff1380ffff01ff0bffff01
-    02ff2bff0580ffff01ff0bffff0102ff05ff2b8080ff0180ffff04ffff04ffff
-    17ff13ffff0181ff80ff3b80ff8080808080ffff010580ff0180ff018080
+    02ff08ffff04ff02ffff04ff0bffff04ffff02ff14ffff04ff02ffff04ff2fff
+    ff04ffff04ff80ff5f80ff8080808080ff8080808080ffff04ff81bfff808080
+    8080808080ffff04ff82017fff808080808080ffff04ffff01ffffff02ffff03
+    ffff09ff05ff1b80ffff0113ffff01ff088080ff0180ffff02ffff03ff2bffff
+    01ff04ffff02ff1affff04ff02ffff04ffff05ffff02ff1cffff04ff02ffff04
+    ff05ffff04ffff04ff2bff8080ff808080808080ffff04ffff05ffff02ff1cff
+    ff04ff02ffff04ff05ffff04ffff04ff13ff3b80ff808080808080ff80808080
+    80ffff0bffff0102ffff06ffff02ff1cffff04ff02ffff04ff05ffff04ffff04
+    ff2bff8080ff808080808080ffff06ffff02ff1cffff04ff02ffff04ff05ffff
+    04ffff04ff13ff3b80ff8080808080808080ffff01ff04ffff04ff3bff1380ff
+    ff0bffff0101ffff02ff1effff04ff02ffff04ffff02ff3bff0580ff80808080
+    808080ff0180ff02ffff03ffff07ff1b80ffff01ff02ff14ffff04ff02ffff04
+    ff05ffff04ff0bff8080808080ffff010b80ff0180ffffff02ffff03ffff09ff
+    05ff1380ffff01ff0101ffff01ff02ff12ffff04ff02ffff04ff05ffff04ff1b
+    ff808080808080ff0180ff02ffff03ff05ffff01ff04ff09ffff02ff1affff04
+    ff02ffff04ff0dffff04ff0bff808080808080ffff010b80ff0180ffff02ffff
+    03ff81dfffff01ff02ffff03ffff02ff12ffff04ff02ffff04ff82011fffff04
+    ff2fff8080808080ffff01ff02ff16ffff04ff02ffff04ff05ffff04ffff04ff
+    37ff0b80ffff04ffff02ffff02ff82011fff0580ffff04ff27ffff04ff82019f
+    ff80808080ffff04ff2fffff04ff81dfff8080808080808080ffff01ff088080
+    ff0180ffff01ff04ff27ffff04ff37ff0b808080ff0180ff02ffff03ffff07ff
+    0580ffff01ff0bffff0102ffff02ff1effff04ff02ffff04ff09ff80808080ff
+    ff02ff1effff04ff02ffff04ff0dff8080808080ffff01ff0bffff0101ff0580
+    80ff0180ff018080
     "
 );
 
 pub const ACTION_LAYER_PUZZLE_HASH: TreeHash = TreeHash::new(hex!(
     "
-    2ad6e558c952fb62de6428fb8d627bcd21ddf37aa8aabb43a8620d98e922a163
+    36c675ccbaf385caeb0d8ee34bffc1ce8b0d4fc9d9dba2b1c2c9bcae75ca7659
     "
 ));
 
