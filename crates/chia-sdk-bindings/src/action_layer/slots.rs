f1ffa4df6f4c52e5b2722321dd633acc404883b7
Yak <y@kuhi.to>
2025-08-21 17:36:17 +0300

    my first macro

--- crates/chia-sdk-bindings/src/action_layer/slots.rs
+++ crates/chia-sdk-bindings/src/action_layer/slots.rs
@@ -3,62 +3,85 @@ use chia_protocol::{Bytes32, Coin};
 use chia_puzzle_types::LineageProof;
 use chia_sdk_driver::Slot;
 use chia_sdk_types::puzzles::{
+    RewardDistributorCommitmentSlotValue, RewardDistributorEntrySlotValue,
     RewardDistributorRewardSlotValue, RewardDistributorSlotNonce, SlotInfo,
 };
 use clvm_utils::ToTreeHash;
 
-pub struct RewardSlot {
-    pub coin: Coin,
-    pub proof: LineageProof,
+macro_rules! define_reward_distributor_slot {
+    ($name:ident, $value_ty:ty, $nonce:expr) => {
+        pub struct $name {
+            pub coin: Coin,
+            pub proof: LineageProof,
 
-    pub nonce: u64,
-    pub launcher_id: Bytes32,
+            pub nonce: u64,
+            pub launcher_id: Bytes32,
 
-    pub value: RewardDistributorRewardSlotValue,
-}
-
-impl RewardSlot {
-    pub fn new(
-        proof: LineageProof,
-        launcher_id: Bytes32,
-        value: RewardDistributorRewardSlotValue,
-    ) -> Result<Self> {
-        let slot = Slot::new(
-            proof,
-            SlotInfo::new(
-                launcher_id,
-                RewardDistributorSlotNonce::REWARD.to_u64(),
-                value.tree_hash().into(),
-                value,
-            ),
-        );
-
-        Ok(Self::from_slot(slot))
-    }
-
-    pub fn value_hash(&self) -> Result<Bytes32> {
-        Ok(self.value.tree_hash().into())
-    }
-
-    pub fn to_slot(self) -> Slot<RewardDistributorRewardSlotValue> {
-        Slot::new(
-            self.proof,
-            SlotInfo::new(
-                self.launcher_id,
-                self.nonce,
-                self.value.tree_hash().into(),
-                self.value,
-            ),
-        )
-    }
-
-    pub fn from_slot(slot: Slot<RewardDistributorRewardSlotValue>) -> Self {
-        Self {
-            coin: slot.coin,
-            proof: slot.proof,
-            nonce: slot.info.nonce,
-            launcher_id: slot.info.launcher_id,
-            value: slot.info.value,
+            pub value: $value_ty,
         }
-    }
+
+        impl $name {
+            pub fn new(
+                proof: LineageProof,
+                launcher_id: Bytes32,
+                value: $value_ty,
+            ) -> Result<Self> {
+                let slot = Slot::new(
+                    proof,
+                    SlotInfo::new(
+                        launcher_id,
+                        $nonce.to_u64(),
+                        value.tree_hash().into(),
+                        value,
+                    ),
+                );
+
+                Ok(Self::from_slot(slot))
+            }
+
+            pub fn value_hash(&self) -> Result<Bytes32> {
+                Ok(self.value.tree_hash().into())
+            }
+
+            pub fn to_slot(self) -> Slot<$value_ty> {
+                Slot::new(
+                    self.proof,
+                    SlotInfo::new(
+                        self.launcher_id,
+                        self.nonce,
+                        self.value.tree_hash().into(),
+                        self.value,
+                    ),
+                )
+            }
+
+            pub fn from_slot(slot: Slot<$value_ty>) -> Self {
+                Self {
+                    coin: slot.coin,
+                    proof: slot.proof,
+                    nonce: slot.info.nonce,
+                    launcher_id: slot.info.launcher_id,
+                    value: slot.info.value,
+                }
+            }
+        }
+    };
 }
+
+define_reward_distributor_slot!(
+    RewardSlot,
+    RewardDistributorRewardSlotValue,
+    RewardDistributorSlotNonce::REWARD
+);
+
+define_reward_distributor_slot!(
+    CommitmentSlot,
+    RewardDistributorCommitmentSlotValue,
+    RewardDistributorSlotNonce::COMMITMENT
+);
+
+define_reward_distributor_slot!(
+    EntrySlot,
+    RewardDistributorEntrySlotValue,
+    RewardDistributorSlotNonce::ENTRY
+);
