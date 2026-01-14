1bf247e395991719bba9e6027cd13370e7abedde
Rigidity <me@rigidnetwork.com>
2026-01-14 15:08:33 -0500

    Depeg from chia crate

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,19 @@ pub use chia_sdk_test as test;
 pub use chia_sdk_types as types;
 pub use chia_sdk_utils as utils;
 
-pub use chia;
 pub use chia_puzzles as puzzles;
+pub use clvm_traits;
+pub use clvm_utils;
 pub use clvmr;
+
+pub mod chia {
+    pub use chia_bls as bls;
+    pub use chia_consensus as consensus;
+    pub use chia_protocol as protocol;
+    pub use chia_puzzle_types as puzzle_types;
+    pub use chia_secp as secp;
+    pub use chia_serde as serde;
+    pub use chia_sha2 as sha2;
+    pub use chia_ssl as ssl;
+    pub use chia_traits as traits;
+}
