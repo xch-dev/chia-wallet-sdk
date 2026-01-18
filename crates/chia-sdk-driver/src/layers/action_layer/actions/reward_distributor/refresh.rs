0c03b3cbb83c9c5ddb8ac73f0cb4796fd7182e14
Yak <y@kuhi.to>
2026-01-18 15:52:55 +0900

    refresh NFT update 100

--- crates/chia-sdk-driver/src/layers/action_layer/actions/reward_distributor/refresh.rs
+++ crates/chia-sdk-driver/src/layers/action_layer/actions/reward_distributor/refresh.rs
@@ -159,107 +159,26 @@ impl RewardDistributorRefreshAction {
         dl_metadata_updater_hash_hash: Bytes32,
         dl_inner_puzzle_hash: Bytes32,
     ) -> Result<(Conditions, Vec<Nft>), DriverError> {
-        let ephemeral_counter =
-            ctx.extract::<HashedPtr>(distributor.pending_spend.latest_state.0)?;
-        let my_id = distributor.coin.coin_id();
-
-        // calculate notarized payments; spend said nfts
-        let my_p2_treehash = Self::my_p2_puzzle_hash(self.launcher_id).into();
-
-        let mut notarized_payments = Vec::with_capacity(current_nfts.len());
-        let mut created_nfts = Vec::with_capacity(current_nfts.len());
-        let mut nft_infos = Vec::with_capacity(current_nfts.len());
-        let mut security_conditions = Conditions::new();
-        let mut total_shares_until_now = 0;
-        for i in 0..current_nfts.len() {
-            let payment_puzzle_hash: Bytes32 = CurriedProgram {
-                program: NONCE_WRAPPER_PUZZLE_HASH,
-                args: NonceWrapperArgs::<(Bytes32, u64), TreeHash> {
-                    nonce: clvm_tuple!(entry_custody_puzzle_hash, nft_shares[i]),
-                    inner_puzzle: my_p2_treehash,
-                },
-            }
-            .tree_hash()
-            .into();
-
-            let np = NotarizedPayment {
-                // NFTs may have different weights in curated NFT mode
-                nonce: clvm_tuple!(
-                    total_shares_until_now,
-                    clvm_tuple!(ephemeral_counter.tree_hash(), my_id)
-                )
-                .tree_hash()
-                .into(),
-                payments: vec![Payment::new(
-                    payment_puzzle_hash,
-                    1,
-                    ctx.hint(payment_puzzle_hash)?,
-                )],
-            };
-            let notarized_payment_ptr = ctx.alloc(&np)?;
-            notarized_payments.push(np);
-            total_shares_until_now += nft_shares[i];
-
-            let nft = current_nfts[i];
-            let offer_nft = current_nfts[i].child(
-                SETTLEMENT_PAYMENT_HASH.into(),
-                None,
-                nft.info.metadata,
-                nft.amount(),
-            );
-            created_nfts.push(offer_nft.child(
-                payment_puzzle_hash,
-                offer_nft.info.current_owner,
-                offer_nft.info.metadata,
-                offer_nft.coin.amount,
-            ));
-
-            nft_infos.push(StakeNftFromDlInfo {
-                nft_launcher_id: nft.info.launcher_id,
-                nft_metadata_hash: nft.info.metadata.tree_hash().into(),
-                nft_metadata_updater_hash_hash: nft
-                    .info
-                    .metadata_updater_puzzle_hash
-                    .tree_hash()
-                    .into(),
-                nft_owner: nft.info.current_owner,
-                nft_transfer_porgram_hash: NftRoyaltyTransferPuzzleArgs::curry_tree_hash(
-                    nft.info.launcher_id,
-                    nft.info.royalty_puzzle_hash,
-                    nft.info.royalty_basis_points,
-                )
-                .into(),
-                nft_shares: nft_shares[i],
-                nft_inclusion_proof: inclusion_proofs[i].clone(),
-            });
-
-            let msg: Bytes32 = ctx.tree_hash(notarized_payment_ptr).into();
-            security_conditions = security_conditions.assert_puzzle_announcement(announcement_id(
-                distributor.coin.puzzle_hash,
-                announcement_id(offer_nft.coin.puzzle_hash, msg),
-            ));
-        }
-
         // spend self
-        let lock_puzzle_solution = RewardDistributorNftsFromDlLockingPuzzleSolution {
-            my_id: distributor.coin.coin_id(),
-            nft_infos,
+        let action_solution = ctx.alloc(&RewardDistributorRefreshNftsFromDlActionSolution {
             dl_root_hash,
             dl_metadata_rest_hash,
             dl_metadata_updater_hash_hash,
             dl_inner_puzzle_hash,
-        };
-        let action_solution = ctx.alloc(&RewardDistributorStakeActionSolution {
-            lock_puzzle_solution,
-            entry_custody_puzzle_hash,
-            existing_slot_cumulative_payout: existing_slot
-                .as_ref()
-                .map_or(-1i128, |s| s.info.value.initial_cumulative_payout as i128),
-            existing_slot_shares: existing_slot.as_ref().map_or(0, |s| s.info.value.shares),
+            total_entry_payout_amount: slots_and_nfts.iter().map(|e| e.entry_payout_amount).sum(),
+            total_shares_delta: slots_and_nfts
+                .iter()
+                .map(|e| e.nfts_total_shares_delta)
+                .sum(),
+            total_payout_rounding_error: slots_and_nfts
+                .iter()
+                .map(|e| e.payout_rounding_error)
+                .sum(),
+            slots_and_nfts,
         })?;
         let action_puzzle = self.construct_puzzle(ctx)?;
 
-        // if needed, spend existing slot
+        // if needed, spend existing slots
         if let Some(existing_slot) = existing_slot {
             let mut msg = (u128::from(existing_slot.info.value.shares)
                 * (distributor
