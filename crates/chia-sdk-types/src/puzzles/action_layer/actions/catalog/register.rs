971db136d3c9c4d32a4b061c1822a5c524f7a7f8
Yak <y@kuhi.to>
2026-01-25 22:50:44 +0900

    register puzzle update for CATalog, savign two bytes each spend

--- crates/chia-sdk-types/src/puzzles/action_layer/actions/catalog/register.rs
+++ crates/chia-sdk-types/src/puzzles/action_layer/actions/catalog/register.rs
@@ -96,63 +96,63 @@ impl NftPack {
     }
 }
 
-pub const CATALOG_REGISTER_PUZZLE: [u8; 1566] = hex!(
+pub const CATALOG_REGISTER_PUZZLE: [u8; 1564] = hex!(
     "
-    ff02ffff01ff02ffff03ffff22ffff0aff82027fff8204ff80ffff0aff8209ff
-    ff82027f80ffff09ffff02ff2effff04ff02ffff04ff82013fff80808080ff82
-    015f8080ffff01ff04ff5fffff02ff32ffff04ff02ffff04ff05ffff04ff8207
-    ffffff04ff82057fffff04ffff02ff3affff04ff02ffff04ff0bffff04ffff0b
-    ffff0101ff82027f80ff8080808080ffff04ffff04ffff04ff28ffff04ff8207
-    ffff808080ffff04ffff04ff24ffff04ffff0effff0172ffff02ff2effff04ff
-    02ffff04ffff04ff82027fff82057f80ff8080808080ff808080ffff04ffff02
-    ff3effff04ff02ffff04ff2fffff04ffff02ff2effff04ff02ffff04ffff04ff
-    8204ffffff04ff8206ffff8209ff8080ff80808080ff8080808080ffff04ffff
-    02ff3effff04ff02ffff04ff2fffff04ffff02ff2effff04ff02ffff04ffff04
-    ff8209ffffff04ff8204ffff820dff8080ff80808080ff8080808080ffff04ff
+    ff02ffff01ff02ffff03ffff22ffff0aff820bffff82027f80ffff0aff8204ff
+    ff820bff80ffff09ffff02ff2effff04ff02ffff04ff8209ffff80808080ff82
+    015f8080ffff01ff04ff5fffff02ff32ffff04ff02ffff04ff05ffff04ff81bf
+    ffff04ff8217ffffff04ffff02ff3affff04ff02ffff04ff0bffff04ffff0bff
+    ff0101ff820bff80ff8080808080ffff04ffff04ffff04ff28ffff04ff81bfff
+    808080ffff04ffff04ff24ffff04ffff0effff0172ffff02ff2effff04ff02ff
+    ff04ffff04ff820bffff8217ff80ff8080808080ff808080ffff04ffff02ff3e
+    ffff04ff02ffff04ff2fffff04ffff02ff2effff04ff02ffff04ffff04ff8202
+    7fffff04ff82037fff8204ff8080ff80808080ff8080808080ffff04ffff02ff
+    3effff04ff02ffff04ff2fffff04ffff02ff2effff04ff02ffff04ffff04ff82
+    04ffffff04ff82027fff8206ff8080ff80808080ff8080808080ffff04ffff02
+    ff2affff04ff02ffff04ff2fffff04ffff02ff2effff04ff02ffff04ffff04ff
+    820bffffff04ff82027fff8204ff8080ff80808080ff8080808080ffff04ffff
+    02ff2affff04ff02ffff04ff2fffff04ffff02ff2effff04ff02ffff04ffff04
+    ff82027fffff04ff82037fff820bff8080ff80808080ff8080808080ffff04ff
     ff02ff2affff04ff02ffff04ff2fffff04ffff02ff2effff04ff02ffff04ffff
-    04ff82027fffff04ff8204ffff8209ff8080ff80808080ff8080808080ffff04
-    ffff02ff2affff04ff02ffff04ff2fffff04ffff02ff2effff04ff02ffff04ff
-    ff04ff8204ffffff04ff8206ffff82027f8080ff80808080ff8080808080ffff
-    04ffff02ff2affff04ff02ffff04ff2fffff04ffff02ff2effff04ff02ffff04
-    ffff04ff8209ffffff04ff82027fff820dff8080ff80808080ff8080808080ff
-    ff04ffff04ff34ffff04ffff0113ffff04ffff0101ffff04ffff02ff82013fff
-    ff04ffff02ff3affff04ff02ffff04ff17ffff04ff82077fffff04ffff0bffff
-    0102ff82027fffff0bffff0101ffff02ff2effff04ff02ffff04ffff04ff8205
-    7fffff04ff82015fff8201bf8080ff808080808080ff808080808080ff8201bf
-    8080ffff04ff8201dfff808080808080ff808080808080808080ff8080808080
-    80808080ffff01ff088080ff0180ffff04ffff01ffffff40ff4633ffff3e42ff
-    02ff02ffff03ff05ffff01ff0bff81e2ffff02ff26ffff04ff02ffff04ff09ff
-    ff04ffff02ff3cffff04ff02ffff04ff0dff80808080ff808080808080ffff01
-    81c280ff0180ffffffffffa04bf5122f344554c53bde2ebb8cd2b7e3d1600ad6
-    31c385a5d7cce23c7785459aa09dcf97a184f32623d11a73124ceb99a5709b08
-    3721e878a16d78f596718ba7b2ffa102a12871fee210fb8619291eaea194581c
-    bd2531e4b23759d225f6806923f63222a102a8d5dd63fba471ebcb1f3e8f7c1e
-    1879b7152a6e7298a91ce119a63400ade7c5ff04ffff04ff38ffff04ff2fffff
-    01ff80808080ffff04ffff02ff36ffff04ff02ffff04ff05ffff04ff17ffff04
-    ffff30ffff30ff0bff2fff8080ff21ffff010180ff808080808080ff5f8080ff
-    ff04ff38ffff04ffff02ff3affff04ff02ffff04ff05ffff04ffff0bffff0101
-    ff0b80ff8080808080ffff04ff80ffff04ffff04ff05ff8080ff8080808080ff
-    0bff81a2ffff02ff26ffff04ff02ffff04ff05ffff04ffff02ff3cffff04ff02
-    ffff04ff07ff80808080ff808080808080ffffff0bff2cffff0bff2cff81c2ff
-    0580ffff0bff2cff0bff81828080ff04ff10ffff04ffff30ff17ffff02ff3aff
-    ff04ff02ffff04ff31ffff04ffff02ff2effff04ff02ffff04ffff04ff31ffff
-    04ff17ff218080ff80808080ffff04ffff02ff3affff04ff02ffff04ff29ffff
-    04ffff0bffff0101ff2980ffff04ff8182ffff04ff39ffff04ffff02ff3affff
-    04ff02ffff04ff25ffff04ffff0bffff0101ff2580ffff04ff8182ffff04ffff
-    02ff3affff04ff02ffff04ff35ffff04ffff02ff2effff04ff02ffff04ffff04
-    ff31ffff04ff17ff218080ff80808080ffff04ff2dffff04ffff0bffff0101ff
-    3d80ff80808080808080ffff04ff0bff8080808080808080ff80808080808080
-    80ff808080808080ffff010180ff808080ffff02ffff03ffff07ff0580ffff01
-    ff0bffff0102ffff02ff2effff04ff02ffff04ff09ff80808080ffff02ff2eff
-    ff04ff02ffff04ff0dff8080808080ffff01ff0bffff0101ff058080ff0180ff
-    04ff34ffff04ffff0112ffff04ff80ffff04ffff02ff3affff04ff02ffff04ff
-    05ffff04ffff0bffff0101ff0b80ff8080808080ff8080808080ff018080
+    04ff8204ffffff04ff820bffff8206ff8080ff80808080ff8080808080ffff04
+    ffff04ff34ffff04ffff0113ffff04ffff0101ffff04ffff02ff8209ffffff04
+    ffff02ff3affff04ff02ffff04ff17ffff04ff821fffffff04ffff0bffff0102
+    ff820bffffff0bffff0101ffff02ff2effff04ff02ffff04ffff04ff8217ffff
+    ff04ff82015fff820dff8080ff808080808080ff808080808080ff820dff8080
+    ffff04ff8201dfff808080808080ff808080808080808080ff80808080808080
+    8080ffff01ff088080ff0180ffff04ffff01ffffff40ff4633ffff3e42ff02ff
+    02ffff03ff05ffff01ff0bff81e2ffff02ff26ffff04ff02ffff04ff09ffff04
+    ffff02ff3cffff04ff02ffff04ff0dff80808080ff808080808080ffff0181c2
+    80ff0180ffffffffffa04bf5122f344554c53bde2ebb8cd2b7e3d1600ad631c3
+    85a5d7cce23c7785459aa09dcf97a184f32623d11a73124ceb99a5709b083721
+    e878a16d78f596718ba7b2ffa102a12871fee210fb8619291eaea194581cbd25
+    31e4b23759d225f6806923f63222a102a8d5dd63fba471ebcb1f3e8f7c1e1879
+    b7152a6e7298a91ce119a63400ade7c5ff04ffff04ff38ffff04ff2fffff01ff
+    80808080ffff04ffff02ff36ffff04ff02ffff04ff05ffff04ff17ffff04ffff
+    30ffff30ff0bff2fff8080ff21ffff010180ff808080808080ff5f8080ffff04
+    ff38ffff04ffff02ff3affff04ff02ffff04ff05ffff04ffff0bffff0101ff0b
+    80ff8080808080ffff04ff80ffff04ffff04ff05ff8080ff8080808080ff0bff
+    81a2ffff02ff26ffff04ff02ffff04ff05ffff04ffff02ff3cffff04ff02ffff
+    04ff07ff80808080ff808080808080ffffff0bff2cffff0bff2cff81c2ff0580
+    ffff0bff2cff0bff81828080ff04ff10ffff04ffff30ff17ffff02ff3affff04
+    ff02ffff04ff31ffff04ffff02ff2effff04ff02ffff04ffff04ff31ffff04ff
+    17ff218080ff80808080ffff04ffff02ff3affff04ff02ffff04ff29ffff04ff
+    ff0bffff0101ff2980ffff04ff8182ffff04ff39ffff04ffff02ff3affff04ff
+    02ffff04ff25ffff04ffff0bffff0101ff2580ffff04ff8182ffff04ffff02ff
+    3affff04ff02ffff04ff35ffff04ffff02ff2effff04ff02ffff04ffff04ff31
+    ffff04ff17ff218080ff80808080ffff04ff2dffff04ffff0bffff0101ff3d80
+    ff80808080808080ffff04ff0bff8080808080808080ff8080808080808080ff
+    808080808080ffff010180ff808080ffff02ffff03ffff07ff0580ffff01ff0b
+    ffff0102ffff02ff2effff04ff02ffff04ff09ff80808080ffff02ff2effff04
+    ff02ffff04ff0dff8080808080ffff01ff0bffff0101ff058080ff0180ff04ff
+    34ffff04ffff0112ffff04ff80ffff04ffff02ff3affff04ff02ffff04ff05ff
+    ff04ffff0bffff0101ff0b80ff8080808080ff8080808080ff018080
     "
 );
 
 pub const CATALOG_REGISTER_PUZZLE_HASH: TreeHash = TreeHash::new(hex!(
     "
-    ddc07891fc010a39139a5e868a420cfe32c1fe6d1b5a2587d4212626ff740d30
+    e044233b38e8f12c8a93ce4a1443f4a67fd89b9e3c53ce2bb6195b6fca3e4d9e
     "
 ));
 
@@ -199,12 +199,12 @@ impl CatalogDoubleTailHashData {
 #[derive(FromClvm, ToClvm, Debug, Clone, PartialEq, Eq)]
 #[clvm(list)]
 pub struct CatalogRegisterActionSolution<P, S> {
-    pub precommitted_cat_maker_data: PuzzleAndSolution<P, S>,
-    pub other_precommit_data: CatalogOtherPrecommitData,
+    pub my_id: Bytes32,
     pub left_data: CatalogDoubleTailHashData,
     pub right_data: CatalogDoubleTailHashData,
+    pub precommitted_cat_maker_data: PuzzleAndSolution<P, S>,
     #[clvm(rest)]
-    pub my_id: Bytes32,
+    pub other_precommit_data: CatalogOtherPrecommitData,
 }
 
 impl Mod for CatalogRegisterActionArgs {
