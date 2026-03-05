245f34a0a91dc35292f7c9edf9bcf13d1c7dfcc2
Rigidity <me@rigidnetwork.com>
2026-03-05 11:17:57 -0500

    Bindings

--- crates/chia-sdk-bindings/src/clear_signing.rs
+++ crates/chia-sdk-bindings/src/clear_signing.rs
@@ -1,4 +1,5 @@
-use chia_protocol::Bytes32;
+use bindy::Result;
+use chia_protocol::{Bytes, Bytes32};
 use chia_sdk_driver::{self as sdk};
 use clvm_utils::TreeHash;
 
@@ -9,8 +10,6 @@ pub struct VaultSpendReveal {
     pub launcher_id: Bytes32,
     pub custody_hash: TreeHash,
     pub delegated_spend: Spend,
-    pub coin_id: Option<Bytes32>,
-    pub genesis_challenge: Option<Bytes32>,
 }
 
 impl From<VaultSpendReveal> for sdk::VaultSpendReveal {
@@ -19,8 +18,44 @@ impl From<VaultSpendReveal> for sdk::VaultSpendReveal {
             launcher_id: value.launcher_id,
             custody_hash: value.custody_hash,
             delegated_spend: value.delegated_spend.into(),
-            coin_id: value.coin_id,
-            genesis_challenge: value.genesis_challenge,
         }
     }
+}
+
+pub fn calculate_vault_puzzle_message(
+    delegated_puzzle_hash: Bytes32,
+    vault_puzzle_hash: Bytes32,
+) -> Result<Bytes> {
+    Ok(sdk::calculate_vault_puzzle_message(
+        delegated_puzzle_hash,
+        vault_puzzle_hash,
+    ))
+}
+
+pub fn calculate_vault_coin_message(
+    delegated_puzzle_hash: Bytes32,
+    vault_coin_id: Bytes32,
+    genesis_challenge: Bytes32,
+) -> Result<Bytes> {
+    Ok(sdk::calculate_vault_coin_message(
+        delegated_puzzle_hash,
+        vault_coin_id,
+        genesis_challenge,
+    ))
+}
+
+pub fn calculate_vault_start_recovery_message(
+    delegated_puzzle_hash: Bytes32,
+    left_side_subtree_hash: Bytes32,
+    recovery_timelock: u64,
+    vault_coin_id: Bytes32,
+    genesis_challenge: Bytes32,
+) -> Result<Bytes> {
+    Ok(sdk::calculate_vault_start_recovery_message(
+        delegated_puzzle_hash,
+        left_side_subtree_hash,
+        recovery_timelock,
+        vault_coin_id,
+        genesis_challenge,
+    ))
 }
