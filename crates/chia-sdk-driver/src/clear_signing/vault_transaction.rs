d93064692de7414213d5bee7ea5969d0d4ac94d2
Rigidity <me@rigidnetwork.com>
2026-04-24 17:21:08 -0400

    Add self transfer test

--- crates/chia-sdk-driver/src/clear_signing/vault_transaction.rs
+++ crates/chia-sdk-driver/src/clear_signing/vault_transaction.rs
@@ -34,8 +34,9 @@ pub struct VaultTransaction {
     /// If this is greater than or equal to the fee paid, you can be sure that the XCH spent for fees will not be
     /// maliciously redirected for some other purpose by the submitter of the transaction after signing.
     pub reserved_fee: u64,
-    /// The launcher id of the vault, based on the spends authorized by the delegated spend.
-    pub launcher_id: Bytes32,
+    /// The launcher id of the vault, based on the spends authorized by the delegated spend. If there were no other spends,
+    /// this is unknowable, unless the signer has this information stored somewhere locally.
+    pub launcher_id: Option<Bytes32>,
     /// The known p2 puzzle hashes of the vault, based on revealed nonces (the first address is included by default).
     pub p2_puzzle_hashes: Vec<Bytes32>,
     /// The delegated puzzle hash that is being signed for.
@@ -89,7 +90,11 @@ impl VaultTransaction {
         let fee_paid = (input_amount - output_amount).try_into()?;
         let received_payments = parse_asserted_requested_payments(facts, allocator)?;
         let launcher_id = find_launcher_id(&summary.linked_spends)?;
-        let p2_puzzle_hashes = calculate_p2_puzzle_hashes(facts, launcher_id);
+        let p2_puzzle_hashes = if let Some(launcher_id) = launcher_id {
+            calculate_p2_puzzle_hashes(facts, launcher_id)
+        } else {
+            Vec::new()
+        };
 
         Ok(Self {
             vault_child: summary.child,
@@ -105,7 +110,7 @@ impl VaultTransaction {
     }
 }
 
-fn find_launcher_id(linked_spends: &[LinkedSpendSummary]) -> Result<Bytes32, DriverError> {
+fn find_launcher_id(linked_spends: &[LinkedSpendSummary]) -> Result<Option<Bytes32>, DriverError> {
     let mut launcher_id = None;
 
     for spend in linked_spends {
@@ -118,10 +123,6 @@ fn find_launcher_id(linked_spends: &[LinkedSpendSummary]) -> Result<Bytes32, Dri
             return Err(DriverError::ConflictingVaultLauncherIds);
         }
     }
-
-    let Some(launcher_id) = launcher_id else {
-        return Err(DriverError::MissingLinkedSpends);
-    };
 
     Ok(launcher_id)
 }
@@ -155,7 +156,7 @@ mod tests {
     use clvm_utils::ToTreeHash;
     use rstest::rstest;
 
-    use crate::{Action, Id, ParsedAsset, ParsedChild, SpendContext, TestVault};
+    use crate::{Action, Id, ParsedAsset, SpendContext, TestVault};
 
     #[derive(Debug, Clone, Copy, PartialEq, Eq)]
     enum AssetKind {
@@ -175,6 +176,7 @@ mod tests {
         ctx: &mut SpendContext,
         alice: &TestVault,
         asset_kind: AssetKind,
+        amount: u64,
     ) -> Result<IssuedAsset> {
         let hidden_puzzle_hash = if matches!(asset_kind, AssetKind::RevocableCat) {
             Some(Bytes32::default())
@@ -186,7 +188,7 @@ mod tests {
             let result = alice.spend(
                 sim,
                 ctx,
-                &[Action::single_issue_cat(hidden_puzzle_hash, 1000)],
+                &[Action::single_issue_cat(hidden_puzzle_hash, amount)],
             )?;
 
             let asset_id = result.outputs.cats[0][0].info.asset_id;
@@ -203,20 +205,23 @@ mod tests {
         })
     }
 
-    fn check_child_asset(
-        child: &ParsedChild,
+    fn check_asset(
+        asset: &ParsedAsset,
         asset_id: Option<Bytes32>,
         hidden_puzzle_hash: Option<Bytes32>,
+        amount: u64,
     ) {
         if let Some(asset_id) = asset_id {
-            let ParsedAsset::Cat(cat) = child.asset else {
+            let ParsedAsset::Cat(cat) = asset else {
                 panic!("Expected CAT child");
             };
             assert_eq!(cat.info.asset_id, asset_id);
             assert_eq!(cat.info.hidden_puzzle_hash, hidden_puzzle_hash);
         } else {
-            assert!(matches!(child.asset, ParsedAsset::Xch(_)));
+            assert!(matches!(asset, ParsedAsset::Xch(_)));
         }
+
+        assert_eq!(asset.coin().amount, amount);
     }
 
     #[rstest]
@@ -236,6 +241,82 @@ mod tests {
             Some(VaultOutput::new(alice.custody_hash().into(), 1))
         );
 
+        // We don't know the launcher id or p2 puzzle hashes, since there were no other spends to reveal them.
+        assert_eq!(tx.launcher_id, None);
+        assert_eq!(tx.p2_puzzle_hashes.len(), 0);
+        assert_eq!(tx.spends.len(), 0);
+
+        Ok(())
+    }
+
+    #[rstest]
+    fn test_clear_signing_vault_info() -> Result<()> {
+        let mut sim = Simulator::new();
+        let mut ctx = SpendContext::new();
+
+        let alice = TestVault::mint(&mut sim, &mut ctx, 1)?;
+
+        let result = alice.spend(
+            &mut sim,
+            &mut ctx,
+            &[Action::send(Id::Xch, alice.puzzle_hash, 1, Memos::None)],
+        )?;
+
+        let tx =
+            VaultTransaction::parse(&mut ctx, result.delegated_spend, result.coin_spends, vec![])?;
+
+        assert_eq!(tx.launcher_id, Some(alice.info.launcher_id));
+        assert_eq!(tx.p2_puzzle_hashes, vec![alice.puzzle_hash]);
+        assert_eq!(tx.spends.len(), 1);
+
+        Ok(())
+    }
+
+    #[rstest]
+    fn test_clear_signing_self_transfer(
+        #[values(AssetKind::Xch, AssetKind::Cat, AssetKind::RevocableCat)] asset_kind: AssetKind,
+    ) -> Result<()> {
+        let mut sim = Simulator::new();
+        let mut ctx = SpendContext::new();
+
+        let alice = TestVault::mint(&mut sim, &mut ctx, 42)?;
+
+        let IssuedAsset {
+            id,
+            asset_id,
+            hidden_puzzle_hash,
+        } = issue_asset(&mut sim, &mut ctx, &alice, asset_kind, 42)?;
+
+        let memos = ctx.memos(&["Hello, world!"])?;
+
+        let result = alice.spend(
+            &mut sim,
+            &mut ctx,
+            &[Action::send(id, alice.puzzle_hash, 42, memos)],
+        )?;
+
+        let tx =
+            VaultTransaction::parse(&mut ctx, result.delegated_spend, result.coin_spends, vec![])?;
+
+        assert_eq!(tx.spends.len(), 1);
+
+        let spend = &tx.spends[0];
+
+        check_asset(&spend.asset, asset_id, hidden_puzzle_hash, 42);
+        assert!(spend.clawback.is_none());
+        assert_eq!(spend.p2_singleton.launcher_id, alice.info.launcher_id);
+        assert_eq!(spend.p2_singleton.nonce, 0);
+        assert_eq!(spend.p2_singleton.p2_puzzle_hash, alice.puzzle_hash);
+        assert_eq!(spend.children.len(), 1);
+
+        let child = &spend.children[0];
+
+        check_asset(&child.asset, asset_id, hidden_puzzle_hash, 42);
+        assert_eq!(child.asset.coin().amount, 42);
+        assert_eq!(child.memos.clawback, None);
+        assert_eq!(child.memos.p2_puzzle_hash, alice.puzzle_hash);
+        assert_eq!(child.memos.human_readable_memos, vec!["Hello, world!"]);
+
         Ok(())
     }
 
@@ -254,7 +335,7 @@ mod tests {
             id,
             asset_id,
             hidden_puzzle_hash,
-        } = issue_asset(&mut sim, &mut ctx, &alice, asset_kind)?;
+        } = issue_asset(&mut sim, &mut ctx, &alice, asset_kind, 1000)?;
 
         let result = alice.spend(
             &mut sim,
@@ -284,9 +365,8 @@ mod tests {
 
         let child = &spend.children[0];
 
-        check_child_asset(child, asset_id, hidden_puzzle_hash);
+        check_asset(&child.asset, asset_id, hidden_puzzle_hash, 1000);
         assert_eq!(child.memos.p2_puzzle_hash, bob_puzzle_hash);
-        assert_eq!(child.asset.coin().amount, 1000);
 
         Ok(())
     }
