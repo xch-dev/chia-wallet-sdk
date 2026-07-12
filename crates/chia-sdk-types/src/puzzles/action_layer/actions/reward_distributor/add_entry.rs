028580e982e7125928547bc2c13286e18e183867
Yak <y@kuhi.to>
2026-07-12 19:38:53 +0300

    update add_rewards

--- crates/chia-sdk-types/src/puzzles/action_layer/actions/reward_distributor/add_entry.rs
+++ crates/chia-sdk-types/src/puzzles/action_layer/actions/reward_distributor/add_entry.rs
@@ -7,31 +7,32 @@ use hex_literal::hex;
 
 use crate::Mod;
 
-pub const REWARD_DISTRIBUTOR_ADD_ENTRY_PUZZLE: [u8; 505] = hex!(
+pub const REWARD_DISTRIBUTOR_ADD_ENTRY_PUZZLE: [u8; 533] = hex!(
     // Rue
     "
-    ff02ffff01ff04ffff04ff819fffff04ff82015fffff04ffff10ff8202dfff82
-    017f80ff8203df808080ffff04ffff04ffff0143ffff04ffff0112ffff04ffff
-    0effff0161ffff0bffff0102ffff0bffff0101ff81bf80ffff0bffff0101ff82
-    017f808080ffff04ffff02ff04ffff04ff06ffff04ff05ffff04ff0bffff04ff
-    8201ffff808080808080ff8080808080ffff04ffff04ffff0133ffff04ffff02
-    ff04ffff04ff06ffff04ff17ffff04ffff0bffff0101ffff0bffff0102ffff0b
-    ffff010180ffff0bffff0102ffff0bffff0101ff81bf80ffff0bffff0102ffff
-    0bffff0101ff8209df80ffff0bffff0101ff82017f8080808080ff8080808080
-    ffff04ff80ffff04ffff04ff81bfff8080ff8080808080ffff04ffff04ffff01
-    55ffff04ffff10ff820bdfff2f80ff808080ff8080808080ffff04ffff04ffff
-    01ff0bffff0102ffff0bffff0182010280ffff0bffff0102ffff0bffff0102ff
-    ff0bffff0182010180ff0580ffff0bffff0102ffff02ff02ffff04ff02ff0780
-    80ffff0bffff010180808080ffff01ff02ffff03ff03ffff01ff0bffff0102ff
-    ff0bffff0182010480ffff0bffff0102ffff0bffff0102ffff0bffff01820101
-    80ff0580ffff0bffff0102ffff02ff02ffff04ff02ff078080ffff0bffff0101
-    80808080ffff01ff0bffff018201018080ff018080ff018080
+    ff02ffff01ff02ffff03ffff15ff82017fff8080ffff01ff04ffff04ff819fff
+    ff04ff82015fffff04ffff10ff8202dfff82017f80ff8203df808080ffff04ff
+    ff04ffff0143ffff04ffff0112ffff04ffff0effff0161ffff0bffff0102ffff
+    0bffff0101ff81bf80ffff0bffff0101ff82017f808080ffff04ffff02ff04ff
+    ff04ff06ffff04ff05ffff04ff0bffff04ff8201ffff808080808080ff808080
+    8080ffff04ffff04ffff0133ffff04ffff02ff04ffff04ff06ffff04ff17ffff
+    04ffff0bffff0101ffff0bffff0102ffff0bffff010180ffff0bffff0102ffff
+    0bffff0101ff81bf80ffff0bffff0102ffff0bffff0101ff8209df80ffff0bff
+    ff0101ff82017f8080808080ff8080808080ffff04ff80ffff04ffff04ff81bf
+    ff8080ff8080808080ffff04ffff04ffff0155ffff04ffff10ff820bdfff2f80
+    ff808080ff8080808080ffff01ff088080ff0180ffff04ffff04ffff01ff0bff
+    ff0102ffff0bffff0182010280ffff0bffff0102ffff0bffff0102ffff0bffff
+    0182010180ff0580ffff0bffff0102ffff02ff02ffff04ff02ff078080ffff0b
+    ffff010180808080ffff01ff02ffff03ff03ffff01ff0bffff0102ffff0bffff
+    0182010480ffff0bffff0102ffff0bffff0102ffff0bffff0182010180ff0580
+    ffff0bffff0102ffff02ff02ffff04ff02ff078080ffff0bffff010180808080
+    ffff01ff0bffff018201018080ff018080ff018080
     "
 );
 
 pub const REWARD_DISTRIBUTOR_ADD_ENTRY_PUZZLE_HASH: TreeHash = TreeHash::new(hex!(
     "
-    a0d70d0f2ba82613b43593b53b610abde3008661d0f1f90c90e23740229c3d96
+    9a25633bc5b34abc08bf75b62ad5d44caa37270065161c1800189aabe2ae45ec
     "
 ));
 
